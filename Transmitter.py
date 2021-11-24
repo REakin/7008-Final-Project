90b8213868c05aef7d8e652f8a5dd5a878b35835
REakin <Eakinryan@hotmail.com>
2021-11-23 18:02:58 -0800

    added to reciver and emulator

--- Transmitter.py
+++ Transmitter.py
@@ -1,31 +1,50 @@
+import yaml
+import socket
+
 def main():
     #create main loop
     while True:
         #create socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #create connection
-        s.bind(('', 8080))
-        #create thread
-        s.listen(1)
-        #create recieve and send
-        #open file
-        f = open('blanchette.txt', 'r')
-        #read file
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind((cfg["TRANSMITTER"]["IP"], cfg["TRANSMITTER"]["PORT"]))
+        f = open(cfg["TargetFile"], 'r')
+
         data = f.read()
+        windowsize = 2
+        seqnum = 0 #the offset number
         #close file
         f.close()
-        #parse data into 60 byte chunks
-        data = [data[i:i+60] for i in range(0, len(data), 60)]
         #create connection
         conn, addr = s.accept()
         print('Connected by', addr)
         while True:
-            #send data
-            for i in data:
-                conn.send(i.encode())
-            #close connection
-            conn.close()
+            #send packet
+            #increase window size by two times the current each time until a maximum of 1028
+            if windowsize < 1024:
+                windowsize = windowsize * 2
+            packetdata = data[seqnum:seqnum+windowsize]
+            seqnum = seqnum + windowsize
+
+            conn.send(packetdata.encode())
+            #set a timeout for the packet if a ack is not recieved
+            conn.settimeout(1)
+            try:
+                #recieve packet
+                ack = conn.recv(1024)
+                #if the ack is correct, continue
+                if ack == b'ACK':
+                    continue
+                #if the ack is incorrect, resend the packet
+                else:
+                    conn.send(packetdata.encode())
+            except socket.timeout:
+                #if the packet times out, resend the packet
+                conn.send(packetdata.encode())
+            #if the packet is sent, continue
+            continue
             break
 
 if __name__ == '__main__':
+    with open('config.yml', 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
     main()
