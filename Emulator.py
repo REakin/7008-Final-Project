b96cf5bfbfa505095d119c29cc2983c5c4428a0f
REakin <Eakinryan@hotmail.com>
2021-11-23 20:51:41 -0800

    added to Transmitter and made Emulator

--- Emulator.py
+++ Emulator.py
@@ -1,4 +1,4 @@
-import yaml
+# import yaml
 import socket
 
 def main():
@@ -8,31 +8,39 @@ def main():
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         #create connection
-        s.bind((cfg["EMULATOR"]["IP"], cfg["EMULATOR"]["T_PORT"]))
-        s2.bind((cfg["EMULATOR"]["IP"], cfg["EMULATOR"]["R_PORT"]))
+        # s.bind((cfg["EMULATOR"]["IP"], cfg["EMULATOR"]["T_PORT"]))
+        s.bind(("127.0.0.1", 5006))
+        # s2.bind((cfg["EMULATOR"]["IP"], cfg["EMULATOR"]["R_PORT"]))
         #create thread
-        s.listen(1)
-        #create recieve and send
-        conn, addr = s.accept()
-        print('Connected by', addr)
         while True:
             #send packet based off address
-            if addr[0] == cfg["RECEIVER"]["IP"]:
-                data = conn.recv(1024)
-                if not data:
+            packet = s.recvfrom(1024)
+            address = packet[1]
+
+            data = packet[0].decode()
+            _switch = data[0]#1
+            _flag = data[1:2]#2
+            _window = data[3:6]#4
+            _seqnum = data[7:22]#16
+            _windowsize = data[23:34]#12
+            _data = data[35:]#payload
+
+            if address[0] == "127.0.0.1":
+                if not _data:
                     break
-                print(data)
-                conn.send(data)
+                s.sendto(b"ACK"+b"0",("127.0.0.1",5005))
+
+
             #recieve packet based off address
-            elif addr[0] == cfg["TRANSMITTER"]["IP"]:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print(data)
-                conn.send(data)
-        conn.close()
+            # elif addr[0] == cfg["TRANSMITTER"]["IP"]:
+            #     data = conn.recv(1024)
+            #     if not data:
+            #         break
+            #     print(data)
+            #     conn.send(data)
+        s.close()
 
 if __name__ == '__main__':
-    with open('config.yml', 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
+    # with open('config.yml', 'r') as ymlfile:
+    #     cfg = yaml.load(ymlfile)
     main()
--- Transmitter.py
+++ Transmitter.py
@@ -1,50 +1,65 @@
-import yaml
+#!/usr/bin/env python3
 import socket
+import json
 
 def main():
     #create main loop
     while True:
         #create socket
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.bind((cfg["TRANSMITTER"]["IP"], cfg["TRANSMITTER"]["PORT"]))
-        f = open(cfg["TargetFile"], 'r')
-
+        # s.bind((cfg["TRANSMITTER"]["IP"], cfg["TRANSMITTER"]["PORT"]))
+        s.bind(("127.0.0.1", 5005))
+        f = open("Blanchette.txt", 'rb')
         data = f.read()
         windowsize = 2
         seqnum = 0 #the offset number
         #close file
         f.close()
-        #create connection
-        conn, addr = s.accept()
-        print('Connected by', addr)
+
+        #start three way handshake
+        #send SYN
+        print("starting three way handshake")
+        s.sendto(b"SYN", ("127.0.0.1", 5006))
+        #recieve ACK
+        packet = s.recvfrom(1059)
+        message = packet[0].decode()
+        address = packet[1]
+
         while True:
             #send packet
-            #increase window size by two times the current each time until a maximum of 1028
-            if windowsize < 1024:
-                windowsize = windowsize * 2
-            packetdata = data[seqnum:seqnum+windowsize]
-            seqnum = seqnum + windowsize
+            #increase window size by two times the current each time until a maximum of 1024
+            _seqnum = '{0:016b}'.format(seqnum)
+            _windowsize = '{0:012b}'.format(windowsize)
 
-            conn.send(packetdata.encode())
+            packetdata = "0"+"01"+_seqnum+_windowsize
+            packetdata = packetdata.encode()
+            packetdata += data[seqnum:seqnum+windowsize]
+            s.sendto(packetdata,("127.0.0.1",5006))
             #set a timeout for the packet if a ack is not recieved
-            conn.settimeout(1)
+            s.settimeout(3)
             try:
                 #recieve packet
-                ack = conn.recv(1024)
+                ack = s.recvfrom(1024)
                 #if the ack is correct, continue
-                if ack == b'ACK':
+                if ack == b'ACK' + str(seqnum).encode():
+                    print("ACK recieved")
+                    seqnum = seqnum + windowsize
+                    if windowsize < 1024:
+                        windowsize = windowsize * 2
                     continue
                 #if the ack is incorrect, resend the packet
                 else:
-                    conn.send(packetdata.encode())
+                    continue
             except socket.timeout:
                 #if the packet times out, resend the packet
-                conn.send(packetdata.encode())
+                print("timeout")
+                windowsize = windowsize / 2
+                if(windowsize < 1):
+                    windowsize = 1
+                continue
             #if the packet is sent, continue
-            continue
-            break
 
 if __name__ == '__main__':
-    with open('config.yml', 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
+    # with open('config.yaml', 'r') as ymlfile:
+    #     cfg = yaml.load(ymlfile)
     main()
